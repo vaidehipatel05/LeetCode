0c33ef8d5a947d1564ed4f1694305dd00af00061
Vaidehi <152042524+vaidehipatel05@users.noreply.github.com>
2024-01-31 20:47:28 -0800

    Time: 0 ms (100.00%), Space: 40 MB (66.12%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class Solution {
+    public static int fib(int n){
+        return fib(n, new HashMap<>());
+    }
+    public static int fib(int n, HashMap<Integer,Integer> memo) {
+        if(n==0 || n==1){
+            return n;
+        }
+        
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        int result = fib(n-1, memo) + fib(n-2,memo);
+        memo.put(n,result);
+        return result;
+    }
+}
